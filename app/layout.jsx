b646bf6a9bd17d20eb90da57fb1c08c005e9c971
csulayao <csulayao.jnj@gmail.com>
2024-08-06 23:15:37 +0800

    Development Setup completed. TODO: Check Tailwind error at globals.css

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@styles/globals.css';
+
+export const metadata = {
+    title: "Portofolia",
+    descriptiion: "Discover the wonderful world of NextJS"
+}
+
+const RootLayout = ({children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient"></div>
+            </div>
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+)
+}
+
+export default RootLayout;
