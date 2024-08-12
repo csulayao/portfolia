b6fbd91b726cc81f2e09d111e9b838269dc05262
Carlo Sulayao <csulayao.jnj@gmail.com>
2024-08-12 18:03:10 +0800

    Still trying to fix the @apply for tailwindcss. Current dev space does not allow for npm to run. Troubleshooting this first.

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            Discover & shar
+            Like & Share
         </h1>
     </section>
 )
