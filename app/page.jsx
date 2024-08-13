579bb904052e74282b00a925176087b536c14c5b
csulayao <csulayao.jnj@gmail.com>
2024-08-13 23:20:03 +0800

    Added new components Form, Fee, Profile, PromptCard Provider. Practice use client protocol.

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,19 @@
-import '@styles/globals.css';
+import Feed from '@components/Feed';
 
 
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            Like & Share
+            Discover and Share
+            <br className="max-md:hidden"/>
+            <br />
+
         </h1>
+        <h2 className="head2_text text-center orange_gradient">AI-Powered Portoflio</h2>
+        <p className="desc text-center">Portfolia is an open-source AI prompting tool for modern world to discover, create, and share the portfolio of Carlo Sulayao.</p>
+
+        <Feed/>
     </section>
 )
 }
