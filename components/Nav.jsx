9566682f033c7de2f2f8c3febbaf31a66b1fb68c
Carlo Sulayao <csulayao.jnj@gmail.com>
2024-08-14 18:03:29 +0800

    Updated padding top for nav. Added a user login state for tracking and adding posts.

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,15 +6,23 @@ import { useState, useEffect } from 'react';
 import {signIn, signOut, useSession, getProviders} from 'next-auth/react';
 
 const Nav = () => {
+    const isUserLoggedIn = true;
+
   return (
-    <nav className="flex-between w-full mb-16">
+    <nav className="flex-between w-full mb-16 pt-3">
         <Link href="/" className="flex gap-2">
             <Image src="/assets/images/logo.svg" alt="Portfolia Logo" width={30} height={30} className="object-contain"/>
             <p className="logo_text">Portfolia</p>
         </Link>
         {/* Mobile Navigation */}
         <div className="sm:flex hidden">
-
+            {isUserLoggedIn ? (
+                <div className="flex gap-3 md:gap-5">
+                    <Link href="/create-promot" className="black_btn">Create Post</Link>
+                </div>
+            ): (
+                <></>
+            )}
         </div>
     </nav>    
 )
