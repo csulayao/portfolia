d75ee09cd93cd207e3e1d51015dbac10a8877c42
csulayao <csulayao.jnj@gmail.com>
2024-08-16 23:05:29 +0800

    Created a MongoDB database, added credentials to .env file, imported connectTodb func.

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { connectToDB } from "@utils/database";
+
 console.log({
   clientId: process.env.GOOGLE_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -17,6 +19,13 @@ const handler = NextAuth({
   async signIn({ profile }) {
     try {
       //serverless -> Lambda -> dynamodb
+      await connectToDB();
+
+      //Check if user already exists
+
+      //If not, create a new user and save
+
+      return true;
     } catch (error) {}
   },
 });
