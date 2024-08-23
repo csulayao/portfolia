7337d8266135f580c4e790e85fc0e4258bdf2cbd
csulayao <csulayao.jnj@gmail.com>
2024-08-23 23:27:25 +0800

    Update create-prompt to include useState and useSession to handle post events for the form.

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+"use client"
+
+import React from 'react';
+import { useState } from 'next-auth/react';
+import { useSession } from 'react/navigation';
+
+import Form from '@components/Form';
 
 const CreatePrompt = () => {
+  const [submitting, SetSubmitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt:'',
+    tag: '',
+  })
+
+  const createPrompt = async (e)=> {
+
+  }
+
   return (
-    <div>CreatePrompt</div>
+    <Form 
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmmit={createPrompt}
+    />
   )
 }
 
