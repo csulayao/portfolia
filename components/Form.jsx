8756bdbe6e6164d972a4af0c33b6a1877bf42ab6
csulayao <csulayao.jnj@gmail.com>
2024-08-23 23:40:03 +0800

    Built additional elements for the form.

--- components/Form.jsx
+++ components/Form.jsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import Link from 'next/link';
 
 const Form = ({
   type,
   post,
   setPost,
   submitting,
-  handleSubmmit,
+  handleSubmit,
 }) => {
   return (
-    <div>Form</div>
+    <section className="w-full mx-w-full flex-start flex-col">
+      <h1 className="head_text text-left"><span className="blue_gradient">{type} Post</span></h1>
+      <p className="desc text-left max-w-md">
+        {type} and share amazing prompts with the world, and let your imagination run wild with any AI-powered portfolio.
+      </p>
+      <form onSubmit={handleSubmit} className="mt-10 w-full max-w-2xl flex"></form>
+    </section>
   )
 }
 
