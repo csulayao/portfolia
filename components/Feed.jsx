7143cdde9db762a4683f2930444e6ecea8cebf93
csulayao <csulayao.jnj@gmail.com>
2024-08-25 00:43:03 +0800

    Updated Feed.jsx to have the basic components to display the feeds.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from 'react'
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({data, handleTagClick}) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const handleSearchChange = (e) => {
+    e.preventDefault;
+  }
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form lassName="relative w-full flex-center">
+        <input 
+          type="text" 
+          placeholder="Search for a portfolio or a username" 
+          value={searchText} 
+          onChange={handleSearchChange}
+          required
+          className="search_input peer" />
+      </form>
+      <PromptCardList 
+        data={[]}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
