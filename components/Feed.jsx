62e3db2108f5589a207403d6b129db0ec112302b
csulayao <csulayao.jnj@gmail.com>
2024-08-26 15:40:00 +0800

    Completed logic and structure for PromptCardList including the API fetch.

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,16 +7,35 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({data, handleTagClick}) => {
   return (
     <div className="mt-16 prompt_layout">
-
+      {data.map((post) => (
+        <PromptCard 
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
     </div>
   )
 }
 
 const Feed = () => {
   const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
   const handleSearchChange = (e) => {
     e.preventDefault;
   }
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    const response = await fetch('/api/prompt');
+    const data = await response.json();
+
+    setPosts(data);
+  }
+
+  fetchPosts();
+}, []);
 
   return (
     <section className="feed">
@@ -30,7 +49,7 @@ const Feed = () => {
           className="search_input peer" />
       </form>
       <PromptCardList 
-        data={[]}
+        data={[posts]}
         handleTagClick={() => {}}
       />
     </section>
