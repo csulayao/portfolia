42485ace034f55b83372e31057e178e94af86cda
csulayao <csulayao.jnj@gmail.com>
2024-08-26 17:22:32 +0800

    Promptcard requires fixing. No creator values are displayed.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -10,13 +10,21 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div>
-          <Image 
-              src={post.creator.Image}
+          {/* <Image 
+              src={post.creator.image}
               alt="user_image"
               width={40}
               height={40}
               className="rounded-full object-contain"
-            />
+            /> */}
+            <div className="flex flex-col">
+              <h3 className="font-satoshi font-semibold text-gray-900">
+                {post.creator}
+              </h3>
+              <p className="font-inter text-sm text-gray 500">
+                {/* {post.creator.email} */}
+              </p>
+            </div>
         </div>
       </div>
     </div>
