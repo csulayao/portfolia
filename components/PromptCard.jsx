505cdf0ac5537ca677aee81329a7c1ab10ffd90f
csulayao <csulayao.jnj@gmail.com>
2024-08-27 23:32:06 +0800

    Update prompcard.jsx to try to fix the render error.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,13 +5,13 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
+const PromptCard = ({prompt, handleTagClick, handleEdit, handleDelete}) => {
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div>
           {/* <Image 
-              src={post.creator.image}
+              src={prompt.creator.image}
               alt="user_image"
               width={40}
               height={40}
@@ -19,10 +19,10 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
             /> */}
             <div className="flex flex-col">
               <h3 className="font-satoshi font-semibold text-gray-900">
-                {post.creator}
+                {/* {prompt.creator} */}
               </h3>
               <p className="font-inter text-sm text-gray 500">
-                {/* {post.creator.email} */}
+                {prompt.creator.email}
               </p>
             </div>
         </div>
