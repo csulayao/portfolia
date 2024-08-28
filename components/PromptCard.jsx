a0e07666ee403813fc60dee6da06406aed6dd51a
csulayao <csulayao.jnj@gmail.com>
2024-08-28 22:34:03 +0800

    Updated all posts reference to prompt.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -10,19 +10,19 @@ const PromptCard = ({prompt, handleTagClick, handleEdit, handleDelete}) => {
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
         <div>
-          {/* <Image 
-              src={prompt.creator.image}
+          <Image 
+              src={prompt.Image}
               alt="user_image"
               width={40}
               height={40}
               className="rounded-full object-contain"
-            /> */}
+            />
             <div className="flex flex-col">
               <h3 className="font-satoshi font-semibold text-gray-900">
-                {/* {prompt.creator} */}
+                {prompt.creator}
               </h3>
               <p className="font-inter text-sm text-gray 500">
-                {prompt.creator.email}
+                {prompt.email}
               </p>
             </div>
         </div>
