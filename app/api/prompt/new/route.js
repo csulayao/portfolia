314c6b047adc82184599c8e2e162299b101eae93
csulayao <csulayao.jnj@gmail.com>
2024-08-30 21:25:22 +0800

    Renamed vars to be consistent across all files.

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -4,13 +4,12 @@ import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
 export const POST = async (req) => {
-  const { userId, image, prompt, tag } = await req.json();
+  const { userId, prompt, tag } = await req.json();
 
   try {
     await connectToDB();
     const newPrompt = new Prompt({
       creator: userId,
-      image,
       prompt,
       tag,
     });
