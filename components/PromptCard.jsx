5cde39ee9269cce9bba1c2ed70cabb1d616ee46c
csulayao <csulayao.jnj@gmail.com>
2024-08-30 21:27:24 +0800

    Added copy button and related methods.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -5,27 +5,43 @@ import Image from "next/image";
 import { useSession } from "next-auth/react";
 import { usePathname, useRouter } from "next/navigation";
 
-const PromptCard = ({prompt, handleTagClick, handleEdit, handleDelete}) => {
+const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
+  const { data: session } = useSession();
+  const pathName = usePathname();
+  const router = useRouter();
+  const [copied, setCopied] = useState("");
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
-        <div>
-          <Image 
-              src={prompt.image}
+        <div className="flex-1 flex justify-start items-center gap-3 cursor-pointer">
+          {/* <Image 
+              src={post.creator.Image}
               alt="user_image"
               width={40}
               height={40}
               className="rounded-full object-contain"
-            />
+            /> */}
             <div className="flex flex-col">
               <h3 className="font-satoshi font-semibold text-gray-900">
-                {prompt.creator}
+                {/* {post.creator} */}
               </h3>
               <p className="font-inter text-sm text-gray 500">
-                {prompt.email}
+                {/* {post.creator.email} */}
+                {post.prompt}
               </p>
             </div>
         </div>
+            <div className="copy_btn" onClick={() => {}}>
+              <Image 
+                src={copied ===post.prompt
+                  ? '/assets/icons.tick.svg'
+                  : 'assets/icons/copy.svg'
+                }
+                width={12}
+                height={12}
+              />
+            </div>
       </div>
     </div>
   )
