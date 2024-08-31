e70555db0bc7b77d41a8f5c8bbb2f16e3cd324e7
csulayao <csulayao.jnj@gmail.com>
2024-08-31 23:09:55 +0800

    Added methods to allow prompts to be clicked.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -45,7 +45,8 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       </div>
       <p className="my-4 font-satoshi text-sm text-gray-700">
         {post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointer">
+      <p className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}>
         {post.tag}
       </p>
     </div>
