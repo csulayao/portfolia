80b46d56313f0887a1ea07157aeb7e431dc77dcf
csulayao <csulayao.jnj@gmail.com>
2024-09-01 16:15:09 +0800

    Renamed profile page as my-profile to resolve declaration issues.

---
+++ app/my-profile/page.jsx
@@ -0,0 +1,30 @@
+"use client"
+
+import { useState, useEffect} from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/Profile';
+
+const MyProfile = () => {
+
+    const handleEdit = () =>{
+
+    }
+
+    const handleDelete = () =>{
+        
+    }
+
+    return (
+        <Profile 
+            name="My"
+            desc="Welcome to your personalized profile page"
+            data={[]}
+            handleEdit={[]}
+            handleDelete={[]}
+        />
+    )
+}
+
+export default MyProfile
