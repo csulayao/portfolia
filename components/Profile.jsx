0bff71ffa93a2bd6aff844a3c0cf514ea54e5610
csulayao <csulayao.jnj@gmail.com>
2024-09-07 11:07:02 +0800

    Updated code formatting for readability.

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -5,22 +5,22 @@ const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
     <section className="w-full">
       <h1 className="head_text text-left">
         <span className="blue_gradient">
-            {name}'s Profile
+            {name} Profile
         </span>
       </h1>
       <p className="desc text-left">
         {desc}
       </p>
-          <div className="mt-10 prompt_layout">
-      {data.map((post) => (
-        <PromptCard 
-          key={post._id}
-          post={post}
-          handleEdit={() => handleEdit && handleEdit(post)}
-          handleDelete={() => handleDelete && handleDelete(post)}
-        />
-      ))}
-    </div>
+      <div className="mt-10 prompt_layout">
+        {data.map((post) => (
+          <PromptCard 
+            key={post._id}
+            post={post}
+            handleEdit={() => handleEdit && handleEdit(post)}
+            handleDelete={() => handleDelete && handleDelete(post)}
+          />
+        ))}
+      </div>
     </section>
   )
 }
