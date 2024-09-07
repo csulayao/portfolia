f38d6242f3fe41caee8c0d962df392763542b46c
csulayao <csulayao.jnj@gmail.com>
2024-09-07 21:50:25 +0800

    Added PATCH endpoint code.

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,4 +1,4 @@
-//GET
+//GET Prompt for Editing
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
@@ -10,12 +10,33 @@ export const GET = async (request, { params }) => {
     if (!prompt) {
       return new Response("Prompt not found", { status: 404 });
     }
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(prompt), { status: 200 });
   } catch (error) {
     return new Response("Failed to get all portfolio", { status: 500 });
   }
 };
 
 //PATCH
+export const PATCH = async (request, { params }) => {
+  const { prompt, tag } = await request.json();
+
+  try {
+    await connectToDB();
+
+    const existingPrompt = await Prompt.findById(params.id);
+
+    if (!existingPrompt)
+      return new Response("Prompt not found", { status: 404 });
+
+    existingPrompt.prompt = prompt;
+    existingPrompt.tag = tag;
+
+    await existingPrompt.save();
+
+    return new Response(JSON.stringify(prompt), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to update prompt.", { status: 500 });
+  }
+};
 
 //DELETE
