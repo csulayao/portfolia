b7228805f74a638af2e6b251c4b93592daaa092b
csulayao <csulayao.jnj@gmail.com>
2024-09-07 10:41:11 +0800

    Updated the default value for name element.

--- app/my-profile/page.jsx
+++ app/my-profile/page.jsx
@@ -32,7 +32,7 @@ const MyProfile = () => {
 
     return (
         <Profile 
-            name="My"
+            name="Carlo's"
             desc="Welcome to your personalized profile page"
             data={posts}
             handleEdit={handleEdit}
