f19f36ed4978a9fd95130f9117381c39aa27de65
csulayao <csulayao.jnj@gmail.com>
2024-09-11 23:04:46 +0800

    Updated image path for copy icon to improve load consistencies.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -50,8 +50,8 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
             <div className="copy_btn" onClick={handleCopy}>
               <Image 
                 src={copied === post.prompt
-                  ? '/assets/icons/tick.svg'
-                  : 'assets/icons/copy.svg'
+                  ? "/assets/icons/tick.svg"
+                  : "/assets/icons/copy.svg"
                 }
                 width={12}
                 height={12}
