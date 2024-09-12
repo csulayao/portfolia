1c719c759571a1f7d6486ab680ac66912816130e
csulayao <csulayao.jnj@gmail.com>
2024-09-12 23:51:24 +0800

    Added missingSuspenseWithCSRBailout: false to try to resolve out of boundary searchParams().

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   experimental: {
     //appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
+    missingSuspenseWithCSRBailout: false,
   },
   images: {
     //domains: ["lh3.googleusercontent.com"], //Removing this as Next 14 has deprected remotepattern
