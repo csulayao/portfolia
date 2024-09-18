b337bb2f8f192723228ca3ae7c3352899094262e
csulayao <csulayao.jnj@gmail.com>
2024-09-18 22:39:35 +0800

    Added favicon in layout to fix missing favicon.

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,10 +11,7 @@ const RootLayout = ({children}) => {
   return (
     <html lang="en">
         <head>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-            <link rel="manifest" href="/site.webmanifest"/>
+            <link rel="icon" href="/icon/?<generated>" type="image/<generated>" sizes="<generated>"/>
         </head>
         <body>
             <Provider>
