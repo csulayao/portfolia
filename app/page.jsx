ed72c2e77713ff4f2c7baf8ad3c8148c7d2c5026
csulayao <csulayao.jnj@gmail.com>
2024-09-20 23:07:30 +0800

    Updated orange reference to indigo.

--- app/page.jsx
+++ app/page.jsx
@@ -5,13 +5,13 @@ const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
         <h1 className="head_text text-center">
-            Discover and Share
+            Explore and Wonder
             <br className="max-md:hidden"/>
             <br />
 
         </h1>
-        <h2 className="head2_text text-center orange_gradient">AI-Powered Portoflio</h2>
-        <p className="desc text-center">Portfolia is an open-source AI prompting tool for modern world to discover, create, and share the portfolio of Carlo Sulayao of D-Kolektiv, Inc.</p>
+        <h2 className="head2_text text-center indigo_gradient">AI-Powered Portoflio</h2>
+        <p className="desc text-center">Portfolia is an open-source project to share the portfolio of Carlo Sulayao of D-Kolektiv, Inc.</p>
             <Feed/>
     </section>
 )
