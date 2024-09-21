b446712c049257182596c707d32d3c76469a0474
csulayao <csulayao.jnj@gmail.com>
2024-09-21 22:29:45 +0800

    Corrected grammatical error.

--- app/page.jsx
+++ app/page.jsx
@@ -4,14 +4,14 @@ import Feed from '@components/Feed';
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        <h1 className="head_text text-center">
+        <h1 className="head_text text-center border-t-slate-800">
             Explore and Wonder
             <br className="max-md:hidden"/>
             <br />
 
         </h1>
         <h2 className="head2_text text-center indigo_gradient">Javascript-Powered Portoflio</h2>
-        <p className="desc text-center">Portfolia is an NextJS and Tailwind project to showcase the body of work by Carlo Sulayao of D-Kolektiv, Inc.</p>
+        <p className="desc text-center">Portfolia is a NextJS and Tailwind project to showcase the body of work by Carlo Sulayao of D-Kolektiv, Inc.</p>
             <Feed/>
     </section>
 )
