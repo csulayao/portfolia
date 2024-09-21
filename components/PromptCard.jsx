88492a32ae00639e8e02ecf4741432cab667f4b6
csulayao <csulayao.jnj@gmail.com>
2024-09-21 11:17:32 +0800

    Updated reference to blue_gradient to tag_gradient.

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -61,7 +61,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
       </div>
       <p className="my-4 font-satoshi text-sm text-gray-900">
         {post.prompt}</p>
-      <p className="font-inter text-sm blue_gradient cursor-pointer"
+      <p className="font-inter text-sm tag_gradient cursor-pointer"
         onClick={() => handleTagClick && handleTagClick(post.tag)}>
         {post.tag}
       </p>
