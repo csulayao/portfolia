652e415467345b9a4a79e1ead77786e98679da8b
csulayao <csulayao.jnj@gmail.com>
2024-09-21 17:34:01 +0800

    Updated styles and elements for footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,15 +1,19 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer className="sm:flex w-full mb-0 pt-10 pb-5 pl-10 bg-slate-950 text-white font-satoshi">
-      <div>&copy; D-Kolektiv, Inc. 2024</div>
-      <div className="pl-10"><Link href="/" className="flex gap-3 md:gap-5">LinkedIn</Link>
-      <Link href="/" className="flex gap-3 md:gap-5">Github</Link>
+    <footer className="flex w-full mb-0 pt-10 pb-5 pl-10 bg-slate-950 text-white font-satoshi">
+      <div className="sm:flex md:flex-1">
+        <span className="flex mr-10">&copy; D-Kolektiv, Inc. 2024</span>
+        <Link href="https://www.linkedin.com/in/carlosulayao" className="flex mr-10">LinkedIn</Link>
+        <Link href="https://github.com/csulayao" className="flex mr-10">Github</Link>
+        <Link href="https://x.com/iamcarlosu" className="flex mr-10">X.com</Link>
+      </div>
+      <div className="pl-10">
+
       </div>
     </footer>
-    //[![LinkedIn](https://img.shields.io/badge/LinkedIn-%230077B5.svg?logo=linkedin&logoColor=white)](https://www.linkedin.com/in/carlosulayao/) [![X](https://img.shields.io/badge/X-black.svg?logo=X&logoColor=white)](https://x.com/iamcarlosu)[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F.svg?logo=Instagram&logoColor=white)](https://instagram.com/iamcarlosu)  
   )
 }
 
