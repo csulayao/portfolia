c59bc34a1300e8528367513dc9280cd830193684
csulayao <csulayao.jnj@gmail.com>
2024-09-23 23:24:26 +0800

    Updated api route for users.

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -2,16 +2,17 @@
 
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
+import Work from "@models/works";
 
 export const GET = async (request, { params }) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({
+    const works = await Work.find({
       creator: params.id,
     }).populate("creator");
 
-    return new Response(JSON.stringify(prompts), { status: 200 });
+    return new Response(JSON.stringify(works), { status: 200 });
   } catch (error) {
     return new Response("Failed to get all portfolio", { status: 500 });
   }
