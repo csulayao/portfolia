af571ff2bfa1e1b1135d5ad1f7be65db6b64991a
csulayao <csulayao.jnj@gmail.com>
2024-09-24 23:03:28 +0800

    Renamed all prompts reference to works.

---
+++ app/api/works/new/route.js
@@ -0,0 +1,25 @@
+//This is how an API Route in NextJS looks like
+
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+import Work from "@models/works";
+
+export const POST = async (req) => {
+  const { userId, worktitle, workurl, workimg, tag } = await req.json();
+
+  try {
+    await connectToDB();
+    const newWork = new Work({
+      creator: userId,
+      worktitle,
+      workurl,
+      workimg,
+      tag,
+    });
+    await newWork.save();
+
+    return new Response(JSON.stringify(newWork), { status: 201 });
+  } catch (error) {
+    return new Response("Failed to create a new work entry", { status: 500 });
+  }
+};
---
+++ app/api/works/route.js
@@ -0,0 +1,15 @@
+//This is the Prompt GET API
+
+import { connectToDB } from "@utils/database";
+import Work from "@models/works";
+export const GET = async (request) => {
+  try {
+    await connectToDB();
+
+    const works = await Work.find({}).populate("creator");
+
+    return new Response(JSON.stringify(works), { status: 200 });
+  } catch (error) {
+    return new Response("Failed to get all portfolio", { status: 500 });
+  }
+};
