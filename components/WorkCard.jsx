8ce7ab13ebc9d69a9bd18874336c48714c08811d
csulayao <csulayao.jnj@gmail.com>
2024-09-28 14:58:37 +0800

    Updated Workcard to include align with the feeds updates.

--- components/WorkCard.jsx
+++ components/WorkCard.jsx
@@ -55,8 +55,7 @@ const WorkCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
         onClick={() => handleTagClick && handleTagClick(post.tag)}>
         {post.tag}
       </p>
-      {/*session?.user.id === post.creator._id && pathName === '/profile' &&*/}
-      {session?.user.id === post.creator._id &&
+      {session?.user.id === post.creator._id && pathName === '/profile' &&
         (
           <div className="mt-5 flex-center gap-4 border-t border-gray-100 pt-3">
             <p 
