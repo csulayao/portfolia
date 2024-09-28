1a6a78f082011bb386929195448537067b69bb24
csulayao <csulayao.jnj@gmail.com>
2024-09-28 12:02:17 +0800

    Updated style for H1 of profile.

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
   return (
     <section className="w-full">
       <h1 className="head_text text-left">
-        <span className="blue_gradient">
+        <span className="indigo_gradient">
             {name}{`'s Profile`}
         </span>
       </h1>
